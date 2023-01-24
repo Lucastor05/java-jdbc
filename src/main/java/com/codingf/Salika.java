946151183fc4e9db6023a0700ced71df109e8744
Lucastor05 <lucasdeniel05@gmail.com>
2023-01-24 19:42:03 +0100

    v1

--- src/main/java/com/codingf/Salika.java
+++ src/main/java/com/codingf/Salika.java
@@ -45,12 +45,13 @@ public class Salika {
         boolean restart = true;
 
         while(restart) {
-            System.out.println("\n\n1. actor\n2. actor_info\n3. address\n4. category\n5. city\n6. country\n7. customer\n8. customer_list\n9. film\n10. film_actor\n11. film_category\n12. film_list\n13. film_text\n14. inventory\n15. language\n16. nicer_but_slower_film_list\n17. payment\n18. rental\n19. sales_by_film_category\n20. sales_by_store\n21. staff\n22. staff_list\n23. store\n24. Exit");
-            System.out.println("Entrez votre choix :");
-            Scanner choice = new Scanner(System.in);
-
             boolean valide = false;
             while (!valide) {
+
+                System.out.println("\n\n1. actor\n2. actor_info\n3. address\n4. category\n5. city\n6. country\n7. customer\n8. customer_list\n9. film\n10. film_actor\n11. film_category\n12. film_list\n13. film_text\n14. inventory\n15. language\n16. nicer_but_slower_film_list\n17. payment\n18. rental\n19. sales_by_film_category\n20. sales_by_store\n21. staff\n22. staff_list\n23. store\n24. Exit");
+                System.out.println("Entrez votre choix :");
+                Scanner choice = new Scanner(System.in);
+
                 if (choice.hasNextInt()) {
                     int choix = choice.nextInt();
                     if (choix <= 24 || choix >= 1) {
@@ -72,45 +73,165 @@ public class Salika {
 
 
     public static void choiceCountry(Connection connection) throws SQLException{
-        System.out.println("\n1. Créer un nouveau pays\n2. Afficher les pays\n3. Mettre à jour un pays\n4. Supprimer un pays\n5. Retour");
-        System.out.println("Entrez votre choix :");
-        Scanner choice = new Scanner(System.in);
+        boolean restart = true;
 
-        boolean valide = false;
-        while(!valide) {
-            if (choice.hasNextInt()){
-                int choix = choice.nextInt();
-                if(choix <= 5 || choix >= 1){
-                    if(choix == 1){
+        while(restart){
 
-                    }else if(choix == 2){
-                        afficheCountry(connection);
-                    }else if(choix == 3){
+            boolean valide = false;
+            while(!valide) {
+                System.out.println("\n1. Créer un nouveau pays\n2. Afficher les pays\n3. Mettre à jour un pays\n4. Supprimer un pays\n5. Retour");
+                System.out.println("Entrez votre choix :");
+                Scanner choice = new Scanner(System.in);
+                if (choice.hasNextInt()){
+                    int choix = choice.nextInt();
+                    if(choix <= 5 || choix >= 1){
+                        if(choix == 1){
+                            boolean newCountryValide = false;
+                            while(!newCountryValide) {
+                                System.out.print("Entrez le nom du pays que vous souhaitez ajouter : ");
+                                Scanner newCountry = new Scanner(System.in);
 
-                    }else if(choix == 4){
+                                if (!newCountry.hasNextInt()) {
+                                    String name = newCountry.next();
+                                    String query = "SELECT * FROM country WHERE country = ?";
+                                    PreparedStatement statement = connection.prepareStatement(query);
+                                    statement.setString(1, name);
+                                    ResultSet resultSet = statement.executeQuery();
 
-                    }else if(choix == 5){
-                        valide = true;
+                                    if (resultSet.next()) {
+                                        System.err.println("Erreur: Le pays existe déjà !!!");
+                                    } else {
+
+                                        query = "INSERT INTO country (country) VALUES (?);";
+                                        statement = connection.prepareStatement(query);
+                                        statement.setString(1, name);
+
+                                        int result = statement.executeUpdate();
+
+                                        if (result == 1) {
+                                            System.out.println("\nLa valeur a été insérée avec succès dans la table country");
+                                        } else {
+                                            System.out.println("\nErreur lors de l'insertion de la valeur dans la table country");
+                                        }
+
+                                        newCountryValide = true;
+                                        valide = true;
+                                    }
+                                }else{
+                                    System.err.println("\nErreur: Le nom ne peut etre un numéro");
+                                }
+                            }
+                        }else if(choix == 2){
+                            afficheCountry(connection);
+                            choiceCountry(connection);
+                        }else if(choix == 3){
+
+                            boolean newCountryValide = false;
+                            while(!newCountryValide) {
+                                afficheCountry(connection);
+
+                                System.out.print("\nEntrez le numéro du pays que vous souhaitez modifier : ");
+                                Scanner newCountry = new Scanner(System.in);
+
+                                if (newCountry.hasNextInt()) {
+                                    int name = newCountry.nextInt();
+                                    String query;
+                                    PreparedStatement statement;
+                                    boolean nameCountryValide = false;
+
+                                    while(!nameCountryValide) {
+                                        System.out.print("Entrez le nouveau nom du pays : ");
+                                        Scanner newCountryName = new Scanner(System.in);
+
+                                        if (!newCountryName.hasNextInt()) {
+                                            String newName = newCountryName.next();
+                                            query = "UPDATE country SET country = ? WHERE country_id = ?";
+                                            statement = connection.prepareStatement(query);
+                                            statement.setString(1, newName);
+                                            statement.setInt(2, name);
+
+                                            int result = statement.executeUpdate();
+
+                                            if (result == 1) {
+                                                System.out.println("\nLa valeur a été modifiée avec succès dans la table country");
+                                            } else {
+                                                System.out.println("\nErreur lors de la modification de la valeur dans la table country");
+                                            }
+                                            nameCountryValide = true;
+                                        }else{
+                                            System.err.println("\nErreur: Le nom ne peut etre un numéro");
+                                        }
+                                    }
+
+                                    newCountryValide = true;
+
+                                }else{
+                                    System.err.println("\nErreur: Le nom ne peut etre un numéro");
+                                }
+                            }
+                        }else if(choix == 4){
+                            boolean newCountryValide = false;
+                            while(!newCountryValide) {
+                                afficheCountry(connection);
+                                System.out.print("Entrez le nom du pays que vous souhaitez supprimer : ");
+                                Scanner newCountry = new Scanner(System.in);
+
+                                if (!newCountry.hasNextInt()) {
+                                    String name = newCountry.next();
+                                    String query = "SELECT * FROM country WHERE country = ?";
+                                    PreparedStatement statement = connection.prepareStatement(query);
+                                    statement.setString(1, name);
+                                    ResultSet resultSet = statement.executeQuery();
+
+                                    if (!resultSet.next()) {
+                                        System.err.println("\nErreur: Le pays n'existe pas !!!");
+                                    } else {
+
+
+                                        query = "DELETE FROM country WHERE country = ?";
+                                        statement = connection.prepareStatement(query);
+                                        statement.setString(1, name);
+
+                                        int result = statement.executeUpdate();
+
+                                        if (result == 1) {
+                                            System.out.println("La valeur a été supprimée avec succès dans la table country");
+                                        } else {
+                                            System.out.println("\nErreur lors de la suppression de la valeur dans la table country");
+                                        }
+
+                                        newCountryValide = true;
+                                    }
+                                }else{
+                                    System.err.println("\nErreur: Le nom ne peut etre un numéro");
+                                }
+                            }
+                            choiceCountry(connection);
+                        }else if(choix == 5){
+                            valide = true;
+                            restart = false;
+                        }
+                    }else {
+                        System.err.println("\nErreur: Le nombre choisi ne correspond à aucun choix proposé !!!");
                     }
-                }else {
-                    System.err.println("Erreur: Le nombre choisi ne correspond à aucun choix proposé !!!");
-                }
 
-            }else {
-                System.err.println("Erreur: Ceci n'est pas un nombre !!!");
+                }else {
+                    System.err.println("\nErreur: Ceci n'est pas un nombre !!!");
+                }
             }
         }
     }
     public static void afficheCountry(Connection connection) throws SQLException {
         Statement stmt= connection.createStatement();
 
-        ResultSet pays= stmt.executeQuery("SELECT country FROM country");
+        ResultSet pays= stmt.executeQuery("SELECT country_id, country FROM country");
         ResultSetMetaData resultMeta = pays.getMetaData();
 
         while(pays.next()){
             for(int i = 1; i <= resultMeta.getColumnCount(); i++) {
-                System.out.println("pays: "+ pays.getObject(i).toString());
+                System.out.print(pays.getObject(i).toString()+". ");
             }
+            System.out.println();
         }
     }
     public static void afficheCity(Connection connection) throws SQLException {
@@ -121,8 +242,9 @@ public class Salika {
 
         while(city.next()){
             for(int i = 1; i <= resultMeta.getColumnCount(); i++) {
-                System.out.println("Ville: "+ city.getObject(i).toString());
+                System.out.print(city.getObject(i).toString()+". ");
             }
+            System.out.println();
         }
     }
 }
